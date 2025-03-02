974c39788f9b5edf1014ccb8779f5b003085cf16
CalvinSti <calvindavid797@gmail.com>
2025-03-02 20:09:47 +0800

    Update game_3_button (2).gd

--- game_3_button (2).gd
+++ game_3_button (2).gd
@@ -4,4 +4,4 @@ func _ready():
 	pressed.connect(_on_game3_button_pressed)
 
 func _on_game3_button_pressed():
-	get_tree().change_scene_to_file("res://games/game3.tscn")
+	get_tree().change_scene_to_file("Ggame3.tscn")
