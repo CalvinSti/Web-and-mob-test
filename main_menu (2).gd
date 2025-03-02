dc3e006087603694e57cf44455d086cf69f565b2
CalvinSti <calvindavid797@gmail.com>
2025-03-02 20:22:44 +0800

    a

--- main_menu (2).gd
+++ main_menu (2).gd
@@ -2,7 +2,7 @@ extends Control
 
 # Dictionary of available games (Button text -> Scene file path)
 var games = {
-	"Game 1": "game1.tscn",
-	"Game 2": "game2.tscn",
-	"Game 3": "game3.tscn"
+	"Game 1": "Game1.tscn",
+	"Game 2": "Game2.tscn",
+	"Game 3": "Game3.tscn"
 }
